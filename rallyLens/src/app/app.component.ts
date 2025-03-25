6ce7e6a1ba1d17a1323a1d491936440feae175b4
RLeandro05 <lligpic577@g.educaand.es>
2025-03-25 12:07:04 +0100

    Cambios menores en enlaces y añadida flecha de subida en caso de hacer scroll hacia abajo en cualquier pestaa de la página

--- rallyLens/src/app/app.component.ts
+++ rallyLens/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rallyLens';
+
+  scrollTopBtn: HTMLElement | null = null;
+
+  ngOnInit(): void {
+    this.scrollTopBtn = document.getElementById('scrollTopBtn');
+  }
+
+  // Mostrar u ocultar el botón al hacer scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (window.scrollY > 200 && this.scrollTopBtn) {
+      this.scrollTopBtn.style.display = 'block';
+    } else if (this.scrollTopBtn) {
+      this.scrollTopBtn.style.display = 'none';
+    }
+  }
+
+  // Función para hacer scroll hacia arriba
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
