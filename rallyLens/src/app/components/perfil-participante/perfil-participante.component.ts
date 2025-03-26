4e6d389064563ab7f782070a8874a9caa67d7b54
RLeandro05 <lligpic577@g.educaand.es>
2025-03-26 12:32:46 +0100

    Funcionalidad de la foto de perfil para que, al pinchar en ella, se pueda ver más grande

--- rallyLens/src/app/components/perfil-participante/perfil-participante.component.ts
+++ rallyLens/src/app/components/perfil-participante/perfil-participante.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './perfil-participante.component.css'
 })
 export class PerfilParticipanteComponent {
+  showModal = false;
+  modalImageSrc = '';
+  profileImage = '/assets/imagenPrincipal2.jpeg';
 
+  userPhotos = [
+    { url: '/assets/imagenPrincipal2.jpeg' }
+    // Agrega más fotos si es necesario
+  ];
+
+  openModal(imageSrc: string) {
+    this.modalImageSrc = imageSrc;
+    this.showModal = true;
+    document.body.style.overflow = 'hidden'; // Deshabilita el scroll
+  }
+
+  closeModal(event?: Event) {
+    if (event) {
+      event.stopPropagation(); // Evita que el click se propague al modal
+    }
+    this.showModal = false;
+    document.body.style.overflow = ''; // Habilita el scroll nuevamente
+  }
 }
