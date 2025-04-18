fa63af89b7805e39fe55d1c9d742dd879609d080
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-04-18 20:23:27 +0200

    Actualizaciones en los cambios de las bases del concurso para hacerlo funcional

--- rallyLens/src/app/components/home/home.component.ts
+++ rallyLens/src/app/components/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent {
   public isAdminLoged: boolean = false;
   public isEditingRules: boolean = false;
 
+  //Valores por defecto para las bases del concurso por primera vez
   public contestRules = {
     allowedFormats: 'PNG, JPG, JPEG',
     maxSize: 20,
@@ -19,37 +20,41 @@ export class HomeComponent {
     maxVotes: 3
   };
 
+  //Variable auxiliar para que, en caso de cancelar los cambios, mantener los originales
   public originalRules: any;
 
   ngOnInit() {
     this.adminLogueado = localStorage.getItem("adminLogueado");
     if (this.adminLogueado) this.isAdminLoged = true;
 
-    //Cargar reglas desde localStorage si existen
+    //Obtener, si existen, las bases del concurso actuales del localstorage
     const savedRules = localStorage.getItem('contestRules');
     if (savedRules) {
       this.contestRules = JSON.parse(savedRules);
     }
   }
 
+  //Función para mostrar el formulario u ocultarlo
   toggleEditRules() {
     this.isEditingRules = !this.isEditingRules;
-    if (this.isEditingRules) {
 
-      //Guardar una copia de las reglas originales para poder cancelar
+    //En caso de que se quiera editar las bases, guardar como copia de seguridad las bases originales
+    if (this.isEditingRules) {
       this.originalRules = { ...this.contestRules };
     }
   }
 
+  //Función para que, al pinchar en Guardar las nuevas bases, se actualice en el localstorage
   saveRules() {
     localStorage.setItem('contestRules', JSON.stringify(this.contestRules));
     this.isEditingRules = false;
 
+    //Mostrar mensaje de éxito
     alert("Las bases del concurso han sido actualizadas correctamente.");
   }
 
+  //Función para que, al cancelar la edición de las bases, se restauren las bases con la copia ya guardada de antemano
   cancelEdit() {
-    //Restaurar las reglas originales
     this.contestRules = { ...this.originalRules };
     this.isEditingRules = false;
   }
