e3fdb1265cfd33f0028523106ebc96ad65fe5f5e
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-04-26 12:36:48 +0200

    Primer diseño base de panel de administración de participantes, cambios en consulta de listado de participantes en el servidor. Modificaciones en header y footer, además del menú lateral de header arreglados. Faltante por ver un responsive más adecuado al cambio de un menú a otro y cambios en el diseño del panel de administración de participantes

--- rallyLens/src/app/components/admin-participantes/admin-participantes.component.ts
+++ rallyLens/src/app/components/admin-participantes/admin-participantes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceParticipanteService } from '../../services/service-participante.service';
 
 @Component({
   selector: 'app-admin-participantes',
@@ -7,9 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './admin-participantes.component.css'
 })
 export class AdminParticipantesComponent {
+  participantes: any[] = [];
 
+  constructor(private serviceParticipantes: ServiceParticipanteService) {}
 
   ngOnInit() {
-      console.log("Entra en admin-participantes");
+    console.log("Entra en admin-participantes");
+
+    this.serviceParticipantes.listarParticipantes().subscribe(
+      (datos: any) => {
+        console.log("Participantes :>> ", datos);
+        if (datos.success) {
+          this.participantes = datos.data;
+        } else {
+          console.error("Error al obtener participantes:", datos.error);
+        }
+      }, 
+      error => console.error("Error al obtener los participantes :>> ", error)
+    );
   }
 }
