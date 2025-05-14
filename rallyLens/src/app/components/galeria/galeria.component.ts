4309b72a61eb5eaee2f0e1bf018410f4124dd0ba
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-05-14 09:24:47 +0200

    Añadida paginación en Galería

--- rallyLens/src/app/components/galeria/galeria.component.ts
+++ rallyLens/src/app/components/galeria/galeria.component.ts
@@ -18,10 +18,15 @@ export class GaleriaComponent {
 
   votosRealizados = new Set<number>();
 
+  fotosPagina: Foto[] = [];
+  paginaActual: number = 1;
+  fotosPorPagina: number = 10; // Puedes ajustar el número según el diseño de la galería
+
+
   constructor(
-    private serviceFotografias: ServiceFotoService, 
+    private serviceFotografias: ServiceFotoService,
     private serviceParticipantes: ServiceParticipanteService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.cargarFotos();
@@ -30,10 +35,11 @@ export class GaleriaComponent {
   cargarFotos() {
     this.serviceFotografias.listarFotos().subscribe(
       fotos => {
-        if(fotos) {
+        if (fotos) {
           this.fotosAceptadas = fotos.filter(foto => foto.estado === "aceptada");
+
+          // Cargar participantes
           this.fotosAceptadas.forEach(foto => {
-            
             this.serviceParticipantes.obtenerParticipanteID(foto.id_participante).subscribe(
               participante => {
                 if (participante) this.mapaFotosParticipantes.set(foto, participante);
@@ -41,28 +47,56 @@ export class GaleriaComponent {
               error => console.error("Error al obtener participante:", error)
             );
           });
+
+          this.actualizarFotosPagina();
         }
-      }, 
+      },
       error => console.error("Error al listar las fotos :>> ", error)
     );
   }
 
+  actualizarFotosPagina() {
+    const inicio = (this.paginaActual - 1) * this.fotosPorPagina;
+    const fin = inicio + this.fotosPorPagina;
+    this.fotosPagina = this.fotosAceptadas.slice(inicio, fin);
+  }
+
+  siguientePagina() {
+    if (this.paginaActual < this.totalPaginas()) {
+      this.paginaActual++;
+      this.actualizarFotosPagina();
+    }
+  }
+
+  anteriorPagina() {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+      this.actualizarFotosPagina();
+    }
+  }
+
+  totalPaginas(): number {
+    return Math.ceil(this.fotosAceptadas.length / this.fotosPorPagina);
+  }
+
+
+
   sumarVoto(idFoto: number, idParticipante: number) {
     console.log("Entra en sumarVoto :>> ", idFoto);
-    
+
     this.serviceFotografias.alternarVoto(idFoto, idParticipante).subscribe(
       respuesta => {
-        if(respuesta.votoRealizado) {
+        if (respuesta.votoRealizado) {
           const fotografiaActualizadaVotos = this.fotosAceptadas.find(foto => foto.id === idFoto);
-          
-          if(fotografiaActualizadaVotos) fotografiaActualizadaVotos.votos += 1;
+
+          if (fotografiaActualizadaVotos) fotografiaActualizadaVotos.votos += 1;
 
           this.votosRealizados.add(idFoto);
-          
+
         } else {
           const fotografiaActualizadaVotos = this.fotosAceptadas.find(foto => foto.id === idFoto);
 
-          if(fotografiaActualizadaVotos) fotografiaActualizadaVotos.votos -= 1;
+          if (fotografiaActualizadaVotos) fotografiaActualizadaVotos.votos -= 1;
 
           this.votosRealizados.delete(idFoto);
         }
