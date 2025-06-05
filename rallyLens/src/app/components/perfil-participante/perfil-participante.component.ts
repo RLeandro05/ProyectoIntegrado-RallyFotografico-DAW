4944f3943afb8fc46d7c58a32e2ba0e0777aa0a6
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-06-05 19:03:39 +0200

    Más ajustes de Seguridad

--- rallyLens/src/app/components/perfil-participante/perfil-participante.component.ts
+++ rallyLens/src/app/components/perfil-participante/perfil-participante.component.ts
@@ -214,7 +214,7 @@ export class PerfilParticipanteComponent {
           else {
             const mensaje =
               respuesta.faltaParticipante || respuesta.noEncuentra ||
-              respuesta.correoExiste || respuesta.noCambia || null;
+              respuesta.correoExiste || respuesta.noCambia || respuesta["telefonoInvalido"] || null;
 
             mensaje ? alert(mensaje) : console.log(respuesta.error);
           }
